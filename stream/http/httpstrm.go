34bd74dfad39526f78969c32cf6d57b0e6f1acaf
xushiwei <x@goplus.org>
2026-02-09 08:14:31 +0800

    Copyright

--- stream/http/httpstrm.go
+++ stream/http/httpstrm.go
@@ -1,17 +1,18 @@
 /*
-Copyright 2021 The GoPlus Authors (goplus.org)
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-	http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+ * Copyright (c) 2026 The XGo Authors (xgo.dev). All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 package http
 
@@ -30,7 +31,7 @@ var (
 
 // -------------------------------------------------------------------------------------
 
-// Open opens a http file object.
+// Open opens the URL and returns the response body as an io.ReadCloser.
 func Open(url string) (io.ReadCloser, error) {
 	resp, err := Get(url)
 	if err != nil {
@@ -39,6 +40,7 @@ func Open(url string) (io.ReadCloser, error) {
 	return resp.Body, nil
 }
 
+// Get sends a GET request to the specified URL.
 func Get(url string) (resp *http.Response, err error) {
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
